ef2e141d49119c0db0053c7c7bc4382eaf556dfc
DevangSharma <debusharma1999@gmail.com>
2020-05-13 16:48:19 +0530

    first commit

---
+++ D-Encrypter main.py
@@ -0,0 +1,13 @@
+
+print("Welcome! to D-Encryptor \nHere you can encode a message for someone else end at the other end one can decrypt "
+      "it as well")
+
+while True:
+    n = input("Press Enter to continue \n")
+
+    if n == "":
+        print('*'*100, "\n")
+        break
+
+    else:
+        print("Wrong key pressed\n")
