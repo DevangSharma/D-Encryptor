a03547d260777edf8d37ada89a6b6a6289db13f2
DevangSharma <debusharma1999@gmail.com>
2020-05-14 12:21:26 +0530

    added new function

--- functions.py
+++ functions.py
@@ -1,19 +1,32 @@
-def test_input(n,chk):
+def test_input(n, chk):
     while True:
         if n == chk:
-            print('*' * 100, "\n")
+            print("\n", '*' * 100, "\n")
             break
 
         else:
             print("Wrong key pressed, Try again\n")
             n = input()
 
-def test_input_range(n,chk1,chk2):
+
+def test_input_range(n, chk1, chk2):
     while True:
         if n >= chk1 or n <= chk2:
-            print('*' * 100, "\n")
+            print("\n", '*' * 100, "\n")
             break
 
         else:
             print("Wrong input , Try again\n")
             n = input()
+
+
+def encrypt():
+    print("You selected Encryption")
+    message = input("Enter your message from line below :\n")
+
+
+def decrypt():
+    print("You selected Decryption")
+    message = input("Enter your message from line below :\n")
+
+
